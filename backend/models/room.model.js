478a303cae099a6fe67f2fea4de5d73d14dfe7d6
WesPalanca <wespalanca@gmail.com>
2026-01-05 20:24:55 -0500

    fix error

--- backend/models/room.model.js
+++ backend/models/room.model.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 
 const roomSchema = mongoose.Schema({
     name: { type: String, required: true },
+    members: [{ type: mongoose.Types.ObjectId, ref: 'User'}],
     createdBy: { type: mongoose.Types.ObjectId, ref: 'User' },
-    lastMessage: { type: mongoose.Types, ObjectId, ref: 'Message' }
+    lastMessage: { type: mongoose.Types.ObjectId, ref: 'Message' }
 }, {
     timestamps: true
 });
 
 const Room = mongoose.model('Room', roomSchema);
 
-export default Room
+export default Room;
