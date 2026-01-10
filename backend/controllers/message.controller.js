25e18072485e2309fe5df11613e97d7e42abb100
WesPalanca <wespalanca@gmail.com>
2026-01-09 21:12:09 -0500

    refactor

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -3,7 +3,7 @@ import Room from '../models/room.model.js';
 import { sendRoomMessageSchema, sendDirectMessageSchema } from "../validation/message.validation.js";
 import log from "../utils/log.js";
 import User from "../models/user.model.js";
-
+import { createDirectMessage, createRoomMessage } from "../services/message.service.js";
 export const sendRoomMessage = async (req, res) => {
     try {
         const result = sendRoomMessageSchema.safeParse(req.body);
@@ -11,21 +11,12 @@ export const sendRoomMessage = async (req, res) => {
             log('ERROR', 'Bad body request', result.error.issues);
             return res.status(400).json({ success: false, message: result.error.issues });
         }
-        const { roomId, message } = result.data;
-        const from = req.user.userId
-        const room = await Room.findById(roomId);
-        if (!room){
-            log('ERROR', 'Room does not exist', roomId);
-            return res.status(404).json({ success: false, message: 'Room does not exist'});
-        }
-        const newMessage = new Message({
-            from: from,
-            roomId: roomId,
-            message: message
-        });
-        await newMessage.save();
-        log('INFO', 'Message sent successfully');
-        return res.status(201).json({ success: true, message: 'Message sent successfully'});
+        const message = await createRoomMessage({ 
+            roomId: result.data.roomId,
+            userId: req.user.userId,
+            message: result.data.message
+        })
+        return res.status(201).json({ success: true, data: message });
 
     }
     catch(err) {
@@ -41,22 +32,12 @@ export const sendDirectMessage = async (req, res) => {
             log('ERROR', 'Bad body request', result.error.issues);
             return res.status(400).json({ success: false, message: result.error.issues });
         }
-        const { to, message } = result.data;
-        const from = req.user.userId;
-        
-        const recipient = await User.findById(to);
-        if (!recipient) {
-            log('ERROR', 'Recipient not found', to);
-            return res.status(404).json({ success: false, message: 'Recipient not found'});
-        }
-        const newMessage = new Message({
-            from: from,
-            to: to,
-            message: message
-        })
-        await newMessage.save();
-        log('INFO', 'Successfully send DM');
-        return res.status(201).json({ success: true, message: 'Successfully send DM'});
+        const message = await createDirectMessage({
+            recipientId: result.data.to,
+            userId: req.user.userId,
+            message: result.data.message,
+        });
+        return res.status(201).json({ success: true, data: message });
     }
     catch(err) {
         log('ERROR', 'internal server error', { message: err.message});
